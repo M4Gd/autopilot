f7d0ab1137ff012ccf4983269df81be3edb49168
Majid <mj_azani@hotmail.com>
2023-03-15 17:11:35 +0330

    directory og bash files changed

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-mkdir -p /var/cmd
-cp ./*.sh /var/cmd/
-chmod +x /var/cmd/*.sh
+mkdir -p /var/cmd/autopilot
+cp ./*.sh /var/cmd/autopilot/
+rm -f /var/cmd/autopilot/setup.sh
+
+chmod +x /var/cmd/autopilot/*.sh
 
 # get a copy of curent crons
 crontab -l > crontabbk
 # add new cron jobs
-echo "*/1 * * * * cd /var/cmd && sh fpm8.sh" >> crontabbk
-echo "*/2 * * * * cd /var/cmd && sh apache2.sh" >> crontabbk
-echo "*/2 * * * * cd /var/cmd && sh mysql.sh" >> crontabbk
+echo "*/1 * * * * cd /var/cmd/autopilot && sh fpm8.sh" >> crontabbk
+echo "*/2 * * * * cd /var/cmd/autopilot && sh apache2.sh" >> crontabbk
+echo "*/2 * * * * cd /var/cmd/autopilot && sh mysql.sh" >> crontabbk
 
 # install new cron file
 crontab crontabbk
